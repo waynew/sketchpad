04fae955ce188e197b932e9c722a0e861c5dc026
Wayne Werner <waynejwerner@gmail.com>
2018-09-21 08:33:27 -0500

    Possibly more correct
    Definitely more basic

--- SketchPad.cs
+++ SketchPad.cs
@@ -6,43 +6,31 @@ using System.Windows.Ink;
 
 public class Sketchpad : Application {
 
-	[STAThread]
-	public static void Main(){
-		var app = new Sketchpad();
+	Window root;
+	InkCanvas canvas;
+
+	protected override void OnStartup(StartupEventArgs e) {
+		base.OnStartup(e);
+
 		Window root = new Window();
 		InkCanvas inkCanvas1 = new InkCanvas();
 
 		root.Title = "Skortchpard";
-		DrawingAttributes inkDA;
-		DrawingAttributes highlighterDA;
-		bool useHIghlighter = false;
 
 		root.ResizeMode = ResizeMode.CanResizeWithGrip;
 		inkCanvas1.Background = Brushes.DarkSlateBlue;
 		inkCanvas1.DefaultDrawingAttributes.Color = Colors.SpringGreen;
+		inkCanvas1.DefaultDrawingAttributes.Height = 10;
+		inkCanvas1.DefaultDrawingAttributes.Width = 10;
+		inkCanvas1.EditingModeInverted = InkCanvasEditingMode.EraseByPoint;
+		inkCanvas1.EditingMode = InkCanvasEditingMode.Ink;
 
 		root.Content = inkCanvas1;
-
-		inkDA = new DrawingAttributes();
-		inkDA.Color = Colors.SpringGreen;
-		inkDA.Height = 5;
-		inkDA.Width = 5;
-		inkDA.FitToCurve = false;
-
-		highlighterDA = new DrawingAttributes();
-		highlighterDA.Color = Colors.Orchid;
-		highlighterDA.IgnorePressure = true;
-		highlighterDA.StylusTip = StylusTip.Rectangle;
-		highlighterDA.Height = 30;
-		highlighterDA.Width = 10;
-
-		inkCanvas1.DefaultDrawingAttributes = inkDA;
-
 		root.Show();
-		app.MainWindow = root;
-		app.Run();
 	}
 
-	void SwitchHighlighter_Click(Object sender, RoutedEventArgs e){
+	[STAThread]
+	public static void Main(){
+		new Sketchpad().Run();
 	}
 }
